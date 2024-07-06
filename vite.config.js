35308401cbb98369185e829270ffc6477b72e7ef
jiwon <semanticist0@gmail.com>
2024-07-06 17:35:47 +0900

    🔧

--- vite.config.js
+++ vite.config.js
@@ -8,36 +8,36 @@ import { typescriptPaths } from "rollup-plugin-typescript-paths";
 const __dirname = path.resolve();
 
 export default defineConfig({
-    plugins: [],
-    resolve: {
-        alias: [
-            {
-                find: "@",
-                replacement: path.resolve(__dirname, "./src"),
-            },
-        ],
+  plugins: [typescript()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+    ],
+  },
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es", "cjs"],
     },
-    build: {
-        manifest: true,
-        minify: true,
-        reportCompressedSize: true,
-        lib: {
-            entry: path.resolve(__dirname, "src/index.ts"),
-            fileName: "index",
-            formats: ["es", "cjs"],
-        },
-        rollupOptions: {
-            external: [],
-            plugins: [
-                typescriptPaths({
-                    preserveExtensions: true,
-                }),
-                typescript({
-                    sourceMap: false,
-                    declaration: true,
-                    outDir: "dist",
-                }),
-            ],
-        },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: "dist",
+        }),
+      ],
     },
+  },
 });
