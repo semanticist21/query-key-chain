817a4c8e3bdbd6253ce9606339f9127e850e2f3c
jiwon <semanticist0@gmail.com>
2024-09-20 09:48:01 +0900

    🔧 prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  importOrder: [
+    "^@public/(.*)$",
+    "^@ui/(.*)$",
+    "^@components/(.*)$",
+    "^@hooks/(.*)$",
+    "^@utils/(.*)$",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@trivago/prettier-plugin-sort-imports",
+  ],
+};
+
+export default config;
