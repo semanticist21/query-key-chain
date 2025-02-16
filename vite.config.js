27d67b73bbd22d03d7d80ac357f3e51709fdeb04
jiwon <semanticist0@gmail.com>
2025-02-16 10:33:23 +0900

    chore: add threshold on coverage

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,12 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'html'],
       exclude: [...coverageConfigDefaults.exclude, 'src/type'],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
     },
   },
   build: {
